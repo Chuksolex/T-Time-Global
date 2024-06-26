c42092413c7e2d2061e60627c1bb32a1535566f8
Chukwuma Oleka <chuks4flourish@gmail.com>
2024-06-26 16:59:33 +0100

    made testimonial slider responsive

--- src/components/slider/TestimonialSlider.jsx
+++ src/components/slider/TestimonialSlider.jsx
@@ -12,7 +12,21 @@ const TestimonialSlider = () => {
     speed: 500,
     slidesToShow: 3,
     slidesToScroll: 1,
-    swipeToSlide: true
+    swipeToSlide: true,
+    responsive: [
+        {
+          breakpoint: 1024, // Adjust breakpoints as needed
+          settings: {
+            slidesToShow: 2,
+          },
+        },
+        {
+          breakpoint: 768,
+          settings: {
+            slidesToShow: 1,
+          },
+        },
+      ],
   };
 
   return (
