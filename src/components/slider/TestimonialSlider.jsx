46233d2c26a3c40d8def13b4b6c8aadde56afb10
Chukwuma Oleka <chuks4flourish@gmail.com>
2024-06-26 15:15:56 +0100

    404 error still

--- src/components/slider/TestimonialSlider.jsx
+++ src/components/slider/TestimonialSlider.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
-import './TestimonialSlider.css'; //
+import './TestimonialSlider.css';
 import { testimonialData } from '../data/testimonialdata';
 
 const TestimonialSlider = () => {
@@ -18,23 +18,17 @@ const TestimonialSlider = () => {
   return (
     <div className="testimonial-slider-container">
       <Slider {...settings}>
-        {/* <div className="testimonial-slide">Testimonial 1</div>
-        <div className="testimonial-slide">Testimonial 2</div>
-        <div className="testimonial-slide">Testimonial 3</div>
-        <div className="testimonial-slide">Testimonial 4</div>
-        <div className="testimonial-slide">Testimonial 5</div> */}
-          {testimonialData.map((testimonial) => (
-            <div key={testimonial.id} className="testimonial-slide">
-              <div className="testimonial-card">
-                <p className="testimonial-message">{testimonial.message}</p>
-                <div className="testimonial-author">
-                  <p className="author-name fw-bold">{testimonial.name}</p>
-                  <p className="author-position">{testimonial.position}</p>
-                </div>
+        {testimonialData.map((testimonial) => (
+          <div key={testimonial.id} className="testimonial-slide">
+            <div className="testimonial-card">
+              <p className="testimonial-message">{testimonial.message}</p>
+              <div className="testimonial-author">
+                <p className="author-name">{testimonial.name}</p>
+                <p className="author-position">{testimonial.position}</p>
               </div>
             </div>
-          ))}
-        
+          </div>
+        ))}
       </Slider>
     </div>
   );
